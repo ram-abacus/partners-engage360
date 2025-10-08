92f5260e9b424e2f0eec0b47a9a17576bd871ada
Abacus Desk <abacusdesk@Abacuss-iMac.local>
2025-10-08 10:04:49 +0530

    GTM & G-Analytical code implimented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   title: "Engage360 - Printing Partners Program",
@@ -15,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {children}
+        <GoogleTagManager gtmId="GTM-MM5PWB94" />
+        <GoogleAnalytics gaId="G-M6HMK9JLLG" />
+      </body>
+
     </html>
   )
 }
